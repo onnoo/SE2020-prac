b96ea0df56bd3400e0f2d81c08867df49658d9bd
onnoo <sweyjw@gmail.com>
2020-03-20 10:42:35 +0900

    update introduce.py

--- introduce.py
+++ introduce.py
@@ -1,4 +1,16 @@
-cmd = input("( 1 : name ) >>> ")
 
-if cmd == "1":
-    print("Jaewoo Yang")
+import getpass
+
+def print_username(name):
+    print(f"Hello {name}!")
+
+def print_introduce(name):
+    print(f"My name is {name}.")
+
+if __name__ == "__main__":
+    cmd = input("Type your command (1: name, 2: introduce) >>> ")
+    uname = getpass.getuser()
+    if cmd == "1":
+        print_username(uname)
+    elif cmd == "2":
+        print_introduce(uname)
